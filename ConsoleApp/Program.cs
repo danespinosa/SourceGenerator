4083f182e27c98d7262c032510419f285069e2d7
Daniel Espinosa <luespino@microsoft.com>
2024-07-30 11:37:01 -0700

    some changes

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,8 +5,13 @@
         static void Main(string[] args)
         {
             HelloFrom("Generated Code");
+            BuildAt();
+            BuildAtTimeProvider();
         }
 
         static partial void HelloFrom(string name);
+
+        static partial void BuildAt();
+        static partial void BuildAtTimeProvider();
     }
 }
